c7b8da11685c9d157bfcc56d4038d338ca1a41f9
Amar Kumar Dubedy <amdubedy@microsoft.com>
2016-05-25 08:19:57 -0700

    Introduction to AutoFixture completed

---
+++ autofixture/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autofixture
+{
+    public class Calculator
+    {
+        public int Value { get; private set; }
+
+        public void Subtract(int number)
+        {
+            this.Value -= number;
+        }
+    }
+}
---
+++ autofixture/autofixture.tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace autofixture.tests
+{
+    using Ploeh.AutoFixture;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Traditional()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            calc.Subtract(1);
+
+            // Assert
+            Assert.IsTrue(calc.Value < 0);
+        }
+
+        [TestMethod]
+        public void AutoFixtureAnonymousData()
+        {
+            // Arrange
+            var calc = new Calculator();
+            var fixture = new Fixture();
+
+            // Act
+            // Anonymous object to generate data
+            calc.Subtract(fixture.Create<int>());
+
+            //Assert
+            Assert.IsTrue(calc.Value < 0);
+        }
+    }
+}
